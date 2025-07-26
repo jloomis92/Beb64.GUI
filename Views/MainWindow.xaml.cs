1822f9ce7505cbb1fdd46ab14b7b18aa8a606994
jloomis92 <jack.loomis@pm.me>
2025-07-26 08:08:13 -0400

    feature: added about window dialog that shows publisher, date, github repo links, and diagnostics.

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -5,5 +5,11 @@ namespace Beb64.GUI.Views
     public partial class MainWindow : Window
     {
         public MainWindow() => InitializeComponent();
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            var about = new Views.AboutWindow { Owner = this };
+            about.ShowDialog();
+        }
     }
 }
