6ff9718882e90286a36ac24e85ce7fc262a34766
jloomis92 <jack.loomis@pm.me>
2025-07-27 16:07:56 -0400

    .

--- Properties/VersionInfo.cs
+++ Properties/VersionInfo.cs
@@ -3,5 +3,5 @@
 // Keep AssemblyVersion stable to avoid binding issues.
 // Bump FileVersion & InformationalVersion for each release.
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
-[assembly: AssemblyInformationalVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.1")]
